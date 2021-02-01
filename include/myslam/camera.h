056c69ac625d4288d18b38eafa57865c3a788e92
liunao123 <812807457@qq.com>
2021-02-01 23:36:11 +0800

    first commit code of slam

---
+++ include/myslam/camera.h
@@ -0,0 +1,25 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "./common_include.h"
+
+namespace myslam
+{
+    //Pinhole RGB-D camera model
+    class Camera
+    {
+    public:
+        typedef std::shared_ptr<Camera> Ptr;
+        // Camera intrinsics
+        float fx_, fy_, cx, cy, depth_scale_;    
+        Camera();
+    };
+    
+    
+    
+}
+
+
+
+
+#endif
